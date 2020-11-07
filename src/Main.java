4bdaa2962d4136958d2db0d5610f413bb1dc2dae
JR <proismaelgans2017@gmail.com>
2020-11-07 02:08:46 -0500

    implementations des proprietes # Partie1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//******************************************************************** //
+// Main.java     Auteur: Gansonre Ismael
+// //  Laucher du programme
+//******************************************************************//
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
--- src/Matrice.java
+++ src/Matrice.java
@@ -1,6 +1,84 @@
-public class Matrice {
+/******************************************************************** //
+ // Matrice.java     Auteur: Gansonre Ismael
+ // //
+ //******************************************************************/
 
-    public static void main(String[] args) {
-	// write your code here
+
+
+
+
+ //CLASSE MATRICE
+
+class Matrice {
+    private  final double[][] matrice;
+
+    public Matrice(double[][] matrice)
+    {
+        this.matrice = matrice;
+    }
+//getter
+    public double getElement(int ligne, int colonne)
+    {
+        return matrice[ligne][colonne];
+    }
+//setter
+    public void setElement(int ligne, int colonne, double element)
+    {
+        matrice[ligne][colonne] = element;
+    }
+
+    public Matrice additionner(Matrice matrice)
+    {
+        return this;
+    }
+
+    public Matrice faireProduitScalaire(Double matriceScalaire)
+    {
+        return this;
+    }
+
+    public Matrice faireProduitMatriciel(Matrice ProduitMatriciel)
+    {
+        return this;
+    }
+//PRORIETES (Trace, Determinants, Transposee ,CoMatrice,MatriceInverse)
+    public double getTrace()
+    {
+        return 1;
+    }
+
+    public double getDeterminant()
+    {
+        return 1;
+    }
+
+    public Matrice getTransposee()
+    {
+        return this;
+    }
+
+    public Matrice getCoMatrice()
+    {
+        return this;
+    }
+
+    public Matrice getMatriceInverse()
+    {
+        return this;
+    }
+
+    public boolean estCarre()
+    {
+        return true;
+    }
+
+    public boolean estTriangulaire()
+    {
+        return true;
+    }
+
+    public boolean estReguliere()
+    {
+        return true;
     }
 }
